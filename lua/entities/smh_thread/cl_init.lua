039ed2d87674d7ff8ccf7e997a573f9353f9c8c9
Winded <hytonen-a@hotmail.com>
2013-10-21 15:20:22 +0300

    Revamped smh_thread net funcs

--- lua/entities/smh_thread/cl_init.lua
+++ lua/entities/smh_thread/cl_init.lua
@@ -1,5 +1,19 @@
 include("shared.lua");
 
+function ENT:Initialize()
+
+	self:SharedInitialize();
+
+end
+
+function ENT._SetPlayer(len)
+
+	local self = net.ReadEntity();
+	self.player = LocalPlayer();
+
+end
+net.Receive("smh_thread_setplayer", ENT._SetPlayer);
+
 function ENT:Draw() return; end
 
 function ENT:DrawTranslucent() return; end
--- lua/entities/smh_thread/init.lua
+++ lua/entities/smh_thread/init.lua
@@ -3,6 +3,8 @@ include("shared.lua");
 AddCSLuaFile("shared.lua");
 AddCSLuaFile("cl_init.lua");
 
+util.AddNetworkString("smh_thread_setplayer");
+
 function ENT:Initialize()
 
 	self:SharedInitialize();
@@ -11,8 +13,19 @@ function ENT:Initialize()
 	self:SetColor(Color(255, 255, 255, 0));
 	self:DrawShadow(false);
 
-	self.player.smh = self;
+	self:_SetPlayer(self.player);
 
-	self.netfuncs.SetPlayer(self.player);
+end
+
+-- Should not be called seperately; self.player should be set before spawning.
+-- This just set's the player's smh variable and sends a net message to the player.
+function ENT:_SetPlayer(player)
+
+	player.smh = self;
+	self.player = player;
+
+	net.Start("smh_thread_setplayer");
+	net.WriteEntity(self);
+	net.Send(player);
 
 end
