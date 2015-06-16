b14270ca42de3657515a9d56aeccde3075862fef
Winded <hytonen-a@hotmail.com>
2015-06-16 07:43:15 +0300

    Removed now unnecessary patching

--- lua/smh/shared.lua
+++ lua/smh/shared.lua
@@ -50,12 +50,3 @@ SMH.DefaultData = {
 	Rendering = false,
 
 };
-
--- Fix bone manipulations.
-hook.Add("PlayerSpawnedRagdoll", "SMHRagdollFix", function(pl, model, ragdoll)
-	for i = 0, ragdoll:GetBoneCount() - 1 do
-		ragdoll:ManipulateBonePosition(i, Vector(0, 0, 0));
-		ragdoll:ManipulateBoneAngles(i, Angle(0, 0, 0));
-		ragdoll:ManipulateBoneScale(i, Vector(1, 1, 1));
-	end
-end);
