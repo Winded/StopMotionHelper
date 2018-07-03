2d8a8c021d248d18f5739de2c22bfb28de39977b
Winded <hytonen-a@hotmail.com>
2018-07-03 19:37:08 +0300

    Added support for ModelScale

---
+++ lua/smh/modifiers/modelscale.lua
@@ -0,0 +1,20 @@
+MOD.Name = "Model scale";
+
+function MOD:Save(player, entity)
+    return {
+        ModelScale = entity:GetModelScale();
+    };
+end
+
+function MOD:LoadGhost(player, entity, ghost, data)
+	self:Load(player, ghost, data);
+end
+
+function MOD:Load(player, entity, data)
+    entity:SetModelScale(data.ModelScale);
+end
+
+function MOD:LoadBetween(player, entity, data1, data2, percentage)
+	local lerpedModelScale = SMH.LerpLinear(data1.ModelScale, data2.ModelScale, percentage);
+	entity:SetModelScale(lerpedModelScale);
+end
