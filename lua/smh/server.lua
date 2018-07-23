04c3736d574de74baae19e6ae600c136872ce635
Winded <hytonen-a@hotmail.com>
2018-07-23 18:35:55 +0300

    Fixed CS lua file downloads

--- lua/smh/server.lua
+++ lua/smh/server.lua
@@ -12,22 +12,29 @@ include("server/positioning.lua");
 AddCSLuaFile("shared.lua");
 AddCSLuaFile("client.lua");
 
-local path, files, dirs;
-
-path = "smh/shared/";
-files, dirs = file.Find(path .. "*.lua", "LUA");
-for _, f in pairs(files) do
-	AddCSLuaFile(path .. f);
+local function FindRecursive(name, path, func)
+    local files, dirs = file.Find(name .. "/*", path);
+    for _, dir in pairs(dirs) do
+        FindRecursive(name .. "/" .. dir, path, func);
+    end
+    for _, f in pairs(files) do
+        func(name .. "/" .. f);
+    end
 end
 
-path = "smh/client/";
-files, dirs = file.Find(path .. "*.lua", "LUA");
-for _, f in pairs(files) do
-	AddCSLuaFile(path .. f);
+local function AddCSPath(path)
+    if string.sub(path, -4) == ".lua" then
+        AddCSLuaFile(path);
+    end
 end
+
+FindRecursive("smh/shared", "LUA", AddCSPath);
+FindRecursive("smh/client", "LUA", AddCSPath);
+FindRecursive("smh/rxlua", "LUA", AddCSPath);
+FindRecursive("smh/bivalues", "LUA", AddCSPath);
 
 hook.Add("PlayerInitialSpawn", "SMHSetup", function(player)
-	SMH.SetupData(player);
+    SMH.SetupData(player);
 end);
 
 Msg("SMH server initialized.\n");
