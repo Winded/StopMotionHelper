89378614cee6d52d166ab6fae87edb439cdce413
Winded <antton.hytonen@outlook.com>
2021-07-26 20:58:36 +0300

    UI WIP

--- lua/autorun/client/derma_utils.lua
+++ lua/autorun/client/derma_utils.lua
@@ -1,17 +1,26 @@
 
-local PANEL = FindMetaTable("Panel");
+local PANEL = FindMetaTable("Panel")
 
 function PANEL:SetRelativePos(otherPanel, x, y)
-	local posX, posY = otherPanel:GetPos();
-	self:SetPos(posX + x, posY + y);
+	local posX, posY = otherPanel:GetPos()
+	self:SetPos(posX + x, posY + y)
+end
+
+-- For DListView
+function PANEL:UpdateLines(lines)
+	self:ClearSelection()
+	self:Clear()
+	for _, line in pairs(lines) do
+		self:AddLine(line)
+	end
 end
 
 -- For number wangs
 function PANEL:GetNumberStep()
-	return self.Step or 1;
+	return self.Step or 1
 end
 function PANEL:SetNumberStep(step)
-	self.Step = step;
-	self.Up.DoClick = function() self:SetValue(self:GetValue() + self.Step); end
-	self.Down.DoClick = function() self:SetValue(self:GetValue() - self.Step); end
+	self.Step = step
+	self.Up.DoClick = function() self:SetValue(self:GetValue() + self.Step) end
+	self.Down.DoClick = function() self:SetValue(self:GetValue() - self.Step) end
 end
--- lua/smh/client/derma/save.lua
+++ lua/smh/client/derma/save.lua
@@ -1,86 +1,85 @@
+local PANEL = {}
 
-local Rx = SMH.Include("rxlua/rx.lua");
-local RxUtils = SMH.Include("shared/rxutils.lua");
+function PANEL:Init()
 
-local function Create(parent)
+	self:SetTitle("Save")
+	self:SetDeleteOnClose(false)
 
-	local panel = vgui.Create("DFrame", parent);
+	self.FileName = vgui.Create("DTextEntry", self)
+	self.FileName.Label = vgui.Create("DLabel", self)
+	self.FileName.Label:SetText("Name")
+	self.FileName.Label:SizeToContents()
 
-	panel:SetTitle("Save");
-	panel:SetDeleteOnClose(false);
-
-	panel.FileName = vgui.Create("DTextEntry", panel);
-	panel.FileName:Bind(SMH.Data, "SaveFileName", "TextEntry");
-	panel.FileName.Label = vgui.Create("DLabel", panel);
-	panel.FileName.Label:SetText("Name");
-	panel.FileName.Label:SizeToContents();
-
-	panel.FileList = vgui.Create("DListView", panel);
-	panel.FileList:SetMultiSelect(false);
-	panel.FileList:AddColumn("Saved scenes");
-
-	panel.Save = vgui.Create("DButton", panel);
-	panel.Save:SetText("Save");
-
-	panel.Delete = vgui.Create("DButton", panel);
-	panel.Delete:SetText("Delete");
-
-	local basePerformLayout = panel.PerformLayout;
-	panel.PerformLayout = function(_, w, h)
-	
-		basePerformLayout(panel, w, h);
-
-		panel:SetSize(250, 250);
-		panel:SetPos(ScrW() / 2 - panel:GetWide() / 2, ScrH() / 2 - panel:GetTall() / 2);
-	
-		panel.FileName:SetPos(5, 45);
-		panel.FileName:SetSize(panel:GetWide() - 5 - 5, 20);
-		panel.FileName.Label:SetPos(5, 30);
-	
-		panel.FileList:SetPos(5, 67);
-		panel.FileList:SetSize(panel:GetWide() - 5 - 5, 150);
-
-		panel.Save:SetPos(panel:GetWide() - 60 - 5, 219);
-		panel.Save:SetSize(60, 20);
-	
-		panel.Delete:SetPos(panel:GetWide() - 60 - 5 - 60 - 5, 219);
-		panel.Delete:SetSize(60, 20);
-	
-	end
-
-	local function addLines(item, lines)
-		item:ClearSelection();
-		item:Clear();
-		for _, line in pairs(lines) do
-			item:AddLine(line);
+	self.FileList = vgui.Create("DListView", self)
+	self.FileList:SetMultiSelect(false)
+	self.FileList:AddColumn("Saved scenes")
+	self.FileList.OnRowSelected = function(_, rowID, row)
+		if not IsValid(row) then
+			return
 		end
+		self.FileName:SetValue(row:GetValue(1))
 	end
-	
-	local fileListStream = Rx.Subject.create();
-	fileListStream:map(function(files) return panel.FileList, files end)
-		:subscribe(addLines);
 
-	local fileSelectStream = Rx.Subject.create();
-	panel.FileList.OnRowSelected = function(self, rowID, row) fileSelectStream(row:GetValue(1)) end
+	self.Save = vgui.Create("DButton", self)
+	self.Save:SetText("Save")
+	self.Save.DoClick = function()
+		self:DoSave()
+	end
 
-	local _, fileStream = RxUtils.bindDPanel(panel.FileName, nil, "OnValueChange");
-
-	local _, saveStream = RxUtils.bindDPanel(panel.Save, nil, "DoClick");
-	local _, deleteStream = RxUtils.bindDPanel(panel.Delete, nil, "DoClick");
-
-	fileSelectStream:subscribe(fileStream);
-
-	return panel, {
-		Input = {
-			FileList = fileListStream,
-		},
-		Output = {
-			File = fileStream,
-			Save = saveStream,
-			Delete = deleteStream,
-		}
-	};
+	self.Delete = vgui.Create("DButton", self)
+	self.Delete:SetText("Delete")
+	self.Save.DoClick = function()
+		self:DoDelete()
+	end
 
 end
 
-return Create;
+function PANEL:PerformLayout(width, height)
+	
+	self.BaseClass.PerformLayout(self, width, height)
+
+	self:SetSize(250, 250)
+	self:SetPos(ScrW() / 2 - self:GetWide() / 2, ScrH() / 2 - self:GetTall() / 2)
+
+	self.FileName:SetPos(5, 45)
+	self.FileName:SetSize(self:GetWide() - 5 - 5, 20)
+	self.FileName.Label:SetPos(5, 30)
+
+	self.FileList:SetPos(5, 67)
+	self.FileList:SetSize(self:GetWide() - 5 - 5, 150)
+
+	self.Save:SetPos(self:GetWide() - 60 - 5, 219)
+	self.Save:SetSize(60, 20)
+
+	self.Delete:SetPos(self:GetWide() - 60 - 5 - 60 - 5, 219)
+	self.Delete:SetSize(60, 20)
+
+end
+
+function PANEL:SetSaves(saves)
+	self.FileList:UpdateFiles(saves)
+end
+
+function PANEL:DoSave()
+	local path = self.FileName:GetValue()
+	if not fileName or fileName == "" then
+		return
+	end
+
+	-- TODO clientside support for loading and saving
+	self:OnSaveRequested(path, false)
+end
+
+function PANEL:DoDelete()
+	local path = self.FileName:GetValue()
+	if not fileName or fileName == "" then
+		return
+	end
+
+	self:OnDeleteRequested(path)
+end
+
+function PANEL:OnSaveRequested(path, saveToClient) end
+function PANEL:OnDeleteRequested(path) end
+
+vgui.Register("SMHSave", PANEL, "DFrame")
