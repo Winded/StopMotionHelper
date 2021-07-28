8778202a8a59a37f31618edc72f37e005e52607b
Winded <antton.hytonen@outlook.com>
2021-07-28 19:53:51 +0300

    World clicker

--- lua/smh/client/derma/world_clicker.lua
+++ lua/smh/client/derma/world_clicker.lua
@@ -1,45 +1,39 @@
+local PANEL = {}
 
-local Rx = SMH.Include("rxlua/rx.lua");
+function PANEL:Init()
 
-local function Setup()
+	self:SetWorldClicker(true)
+	self.m_bStretchToFit = true
 
-	local clicker = vgui.Create("EditablePanel");
+	self:SetPos(0, 0)
+	self:SetSize(ScrW(), ScrH())
 
-	clicker:SetWorldClicker(true);
-	clicker.m_bStretchToFit = true;
+	self:MakePopup()
+	self:SetVisible(false)
 
-	clicker:SetPos(0, 0);
-	clicker:SetSize(ScrW(), ScrH());
-
-	clicker:MakePopup();
-	clicker:SetVisible(false);
-
-	local mousePressStream = Rx.Subject.create();
-	clicker.OnMousePressed = function(self, mousecode) mousePressStream(mousecode) end
-	
-	local selectorStream = mousePressStream
-		:filter(function(mousecode) return mousecode == MOUSE_RIGHT end)
-		:map(function(mousecode) return util.TraceLine(util.GetPlayerTrace(LocalPlayer())) end);
-
-	local visibilityStream = Rx.Subject.create();
-	visibilityStream:subscribe(function(value)
-		if not value then
-			RememberCursorPosition();
-		end
-		clicker:SetVisible(value);
-		if value then
-			RestoreCursorPosition();
-		end
-	end);
-
-	return clicker, {
-		Input = {
-			Visibility = visibilityStream,
-		},
-		Output = {
-			Selector = selectorStream,
-		}
-	};
 end
 
-return Setup;
+function PANEL:SetVisible(visible)
+	if not visible then
+		RememberCursorPosition()
+	end
+	self.BaseClass.SetVisible(self, visible)
+	if visible then
+		RestoreCursorPosition()
+	end
+end
+
+function PANEL:OnMousePressed(mousecode)
+	if mousecode ~= MOUSE_RIGHT then
+		return
+	end
+
+	local trace = util.TraceLine(util.GetPlayerTrace(LocalPlayer()))
+	if IsValid(trace.Entity) then
+		self:OnEntitySelected(trace.Entity)
+	end
+end
+
+function PANEL:OnEntitySelected(entity) end
+
+vgui.Register("SMHWorldClicker", PANEL, "DFrame")
