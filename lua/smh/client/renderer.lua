ac984c10efb026223cdcd2a7a3e2fb071555f6f6
penolakushari <45951611+penolakushari@users.noreply.github.com>
2021-08-24 19:17:20 +0300

    Fix to the smh_makescreenshot/jpeg not working correctly

--- lua/smh/client/renderer.lua
+++ lua/smh/client/renderer.lua
@@ -2,29 +2,42 @@ local UseScreenshot = false
 local TimerName = "SMHRender"
 local Rendering = false
 
+local MGR = {}
+
+function MGR.IsRendering()
+    return IsRendering
+end
+
+function MGR.Stop()
+	if timer.Exists(TimerName) then
+		timer.Remove(TimerName)
+	end
+
+	LocalPlayer():EmitSound("buttons/button1.wav");
+
+    IsRendering = false
+end
+
 local function RenderTick()
+	local newPos = SMH.State.Frame + 1
+	if newPos > SMH.State.PlaybackLength then
+		MGR.Stop()
+		return
+	end
 
 	local command = "jpeg"
 	if UseScreenshot then
 		command = "screenshot"
 	end
 
-    RunConsoleCommand(command)
+	timer.Simple(0.2, function()
+		RunConsoleCommand(command)
 
-	local newPos = SMH.State.Frame + 1
-	if newPos >= SMH.State.PlaybackLength then
-		MGR.Stop()
-		return
-	end
+		timer.Simple(0.4, function()
+			SMH.Controller.SetFrame(newPos)
+		end)
+	end)
 
-    SMH.Controller.SetFrame(newPos)
-
-end
-
-local MGR = {}
-
-function MGR.IsRendering()
-    return IsRendering
 end
 
 function MGR.Start(useScreenshot)
@@ -36,16 +49,6 @@ function MGR.Start(useScreenshot)
 
 	timer.Create(TimerName, 1, 0, RenderTick)
     IsRendering = true
-end
-
-function MGR.Stop()
-	if timer.Exists(TimerName) then
-		timer.Remove(TimerName)
-	end
-
-	LocalPlayer():EmitSound("buttons/button1.wav");
-
-    IsRendering = false
 end
 
 SMH.Renderer = MGR
