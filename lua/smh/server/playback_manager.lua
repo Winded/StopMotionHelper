7d67a1cbf6d00c5c9f06a636e0c6a528fef4d928
penolakushari <45951611+penolakushari@users.noreply.github.com>
2021-11-02 23:29:49 +0300

    Added smooth playback setting

--- lua/smh/server/playback_manager.lua
+++ lua/smh/server/playback_manager.lua
@@ -2,6 +2,41 @@ local ActivePlaybacks = {}
 
 local MGR = {}
 
+local function PlaybackSmooth(player, playback, settings)
+    playback.Timer = playback.Timer + FrameTime()
+    local timePerFrame = 1 / playback.PlaybackRate
+
+    playback.CurrentFrame = playback.Timer / timePerFrame + playback.StartFrame
+    if playback.CurrentFrame > playback.EndFrame then
+        playback.CurrentFrame = 0
+        playback.StartFrame = 0
+        playback.Timer = 0
+    end
+
+    for entity, keyframes in pairs(SMH.KeyframeData.Players[player].Entities) do
+        local prevKeyframe, nextKeyframe, _ = SMH.GetClosestKeyframes(keyframes, playback.CurrentFrame)
+
+        if not prevKeyframe then continue end
+
+        if prevKeyframe.Frame == nextKeyframe.Frame then
+            for name, mod in pairs(SMH.Modifiers) do
+                if prevKeyframe.Modifiers[name] and nextKeyframe.Modifiers[name] then
+                    mod:Load(entity, prevKeyframe.Modifiers[name], settings);
+                end
+            end
+        else
+            local lerpMultiplier = ((playback.Timer + playback.StartFrame * timePerFrame) - prevKeyframe.Frame * timePerFrame) / ((nextKeyframe.Frame - prevKeyframe.Frame) * timePerFrame)
+            lerpMultiplier = math.EaseInOut(lerpMultiplier, prevKeyframe.EaseOut, nextKeyframe.EaseIn)
+
+            for name, mod in pairs(SMH.Modifiers) do
+                if prevKeyframe.Modifiers[name] and nextKeyframe.Modifiers[name] then
+                    mod:LoadBetween(entity, prevKeyframe.Modifiers[name], nextKeyframe.Modifiers[name], lerpMultiplier, settings);
+                end
+            end
+        end
+    end
+end
+
 function MGR.SetFrame(player, newFrame, settings)
     if not SMH.KeyframeData.Players[player] then
         return
@@ -43,16 +78,20 @@ end
 
 hook.Add("Think", "SMHPlaybackManagerThink", function()
     for player, playback in pairs(ActivePlaybacks) do
-        playback.Timer = playback.Timer + FrameTime()
-        local timePerFrame = 1 / playback.PlaybackRate
-        if playback.Timer >= timePerFrame then
-            playback.CurrentFrame = math.floor(playback.Timer / timePerFrame) + playback.StartFrame
-            if playback.CurrentFrame > playback.EndFrame then
-                playback.CurrentFrame = 0
-                playback.StartFrame = 0
-                playback.Timer = 0
+        if not playback.Settings.SmoothPlayback then
+            playback.Timer = playback.Timer + FrameTime()
+            local timePerFrame = 1 / playback.PlaybackRate
+            if playback.Timer >= timePerFrame then
+                playback.CurrentFrame = math.floor(playback.Timer / timePerFrame) + playback.StartFrame
+                if playback.CurrentFrame > playback.EndFrame then
+                    playback.CurrentFrame = 0
+                    playback.StartFrame = 0
+                    playback.Timer = 0
+                end
+                MGR.SetFrame(player, playback.CurrentFrame, playback.Settings)
             end
-            MGR.SetFrame(player, playback.CurrentFrame, playback.Settings)
+        else
+            PlaybackSmooth(player, playback, playback.Settings)
         end
     end
 end)
